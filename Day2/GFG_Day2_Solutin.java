41e68ae5243efe67454d91accb4d49cf9773144f
danish04dz <mohd.692003@gmail.com>
2025-01-07 00:16:53 +0530

    Day 1

---
+++ Day2/GFG_Day2_Solutin.java
@@ -0,0 +1,44 @@
+public class GFG_Day2_Solutin {
+    static void pushZerosToEnd (int arr []){
+
+        int count=0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i]!=0){
+                arr[count]=arr[i];
+                count++;
+            }
+
+          /*   if(i==arr.length-1 ){
+                while (count<arr.length){
+                    arr[count]=0;
+                    count++;
+               }
+            }
+
+            */
+            
+        }
+
+        while (count<arr.length) {
+            arr[count]=0;
+            count++;
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+       int arr [] = { 12, 0, 1, 0, 34, 1 };
+       // int arr[] = {0, 0, 0};
+
+        
+        pushZerosToEnd(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            
+        }
+    }
+}
