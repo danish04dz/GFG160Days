84a5ed0026b398a6d3458fb39d76bb6def8a81e0
danish04dz <mohd.692003@gmail.com>
2025-01-07 13:45:42 +0530

    Day 2

--- Day2/GFG_Day2_Solutin.java
+++ Day2/GFG_Day2_Solutin.java
@@ -27,6 +27,21 @@ public class GFG_Day2_Solutin {
             
         }
 
+       // second aproch to reduce the execution time time 
+
+      /* for (int i = 0; i < arr.length; i++) {
+
+        if(arr[i]!=0){
+            if(count!=i){
+                int temp= arr[count];
+                arr[count]=arr[i];
+                arr[i]=temp;
+            }
+
+            count++;
+        }
+    }
+ */
     }
 
     public static void main(String[] args) {
